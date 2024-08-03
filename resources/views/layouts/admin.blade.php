4dcc54732641ef401555904633e61afe04a02ea5
meowqr <ivanastana2020@mail.ru>
2024-08-03 22:54:33 +0600

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -161,7 +161,7 @@
                     <strong><i class="fa fa-fw {{ $appIsGit ? 'fa-git-square' : 'fa-code-fork' }}"></i></strong> {{ $appVersion }}<br />
                     <strong><i class="fa fa-fw fa-clock-o"></i></strong> {{ round(microtime(true) - LARAVEL_START, 3) }}s
                 </div>
-                Copyright &copy; 2015 - {{ date('Y') }} <a href="https://pyro.host">Pyro Inc.</a> and <a href="https://pterodactyl.io/">parent collaborators</a>.
+                Copyright &copy; {{ date('Y') }} <a href="https://softcore.pro">Softcore.</a> and <a href="https://pterodactyl.io/">parent collaborators</a>.
             </footer>
         </div>
         @section('footer-scripts')
